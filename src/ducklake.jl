527c985f899de6eafe9978b94a5dbe60edfbcf40
Kobus Herbst <kobush@gmail.com>
2025-06-24 04:11:20 +0000

    Refactor ducklake.jl and update ducklake.sql for improved database handling and metadata schema

--- src/ducklake.jl
+++ src/ducklake.jl
@@ -6,9 +6,16 @@ using DataFrames
 db = DuckDB.DB()
 conn = DBInterface.connect(db)
 try
-  global df = DuckDB.query(db,"SELECT version() as duckdb_version;") |> DataFrame
+  df = DuckDB.query(db,"SELECT version() as duckdb_version;") |> DataFrame
+  show(df)
+  println()
+  DBInterface.execute(conn, "LOAD 'ducklake';")
+  # DBInterface.execute(conn, "ATTACH 'ducklake:sqlite:/home/kherbst/repos/RDALake.jl/database/metadata.sqlite' AS my_ducklake")
+  # DBInterface.execute(conn, "CREATE TABLE my_ducklake.demo (i INTEGER);")
+  # DBInterface.execute(conn, "INSERT INTO my_ducklake.demo VALUES (42), (43);")
+  # df = DuckDB.query(conn, "FROM my_ducklake.demo;")
   show(df)
 finally
-    DBInterface.close(conn)
-    DuckDB.close(db)
+  DBInterface.close(conn)
+  DuckDB.close(db)
 end
