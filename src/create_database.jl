b24f76091c4f4d4a25e9ef0568c265c405ef2652
Kobus Herbst <kobusherbst@users.noreply.github.com>
2025-06-29 05:09:10 +0200

    Implement code changes to enhance functionality and improve performance

--- src/create_database.jl
+++ src/create_database.jl
@@ -3,4 +3,4 @@ using ConfigEnv
 
 dotenv()
 
-@time createdatabase(ENV["RDA_SERVER"], ENV["RDA_DBNAME"], replace=true, sqlite=false)
+@time createdatabase(ENV["RDA_DATABASE_PATH"], ENV["RDA_DBNAME"], replace=true, sqlite=true)
